54518d2cd25cc4bc9b8bc6ba7f62705b148d82a7
irfan andriansyah <irfanandriansyah10@gmail.com>
2021-08-03 16:24:58 +0700

    test(unit-test): create test my-pokemon features

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,8 @@
 import ReactDOM from 'react-dom';
 
 import '@/library/styles/css/style.css';
+import App from '@/apps';
 import { reportWebVitals } from '@/helper';
-
-import App from './app';
 
 ReactDOM.render(<App />, document.getElementById(`root`));
 
